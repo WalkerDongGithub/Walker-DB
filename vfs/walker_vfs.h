1479159cbab13425bebc5a11a9d1aa95ac13baa0
Walker <66550224+WalkerDongGithub@users.noreply.github.com>
2021-12-06 15:44:47 +0800

    Update walker_vfs.h

--- vfs/walker_vfs.h
+++ vfs/walker_vfs.h
@@ -16,8 +16,8 @@
 /**
  * 根据 sqlite 提供的官方文档，一般 vfs 都实现了 sqlite_vfs 接口用来实现大量的系统调用，我们只实现其中一些功能
  * 以在不影响使用的情况下，尽可能的复现sqlite的体系结构。
- * 本文仿照 sqlite 的模式，设计了类似的 vfs 系统
  *
+ * 接口声明，如无特殊情况，结构体中 walker_ 开头的所有的函数都以第二个参数作为 status 输入。
  * 如果没有特殊的输入参数，则按照默认参数 default 传入，程序也根据默认选择进行运行。
  */
 
@@ -26,8 +26,6 @@ typedef struct walker_file           walker_file;
 typedef struct walker_file_function  walker_file_function;
 
 struct walker_vfs {
-
-    walker_string vfs_name;
 
     walker_status (*xOpen)(
             walker_vfs*,
@@ -44,10 +42,7 @@ struct walker_vfs {
 
 struct walker_file {
     walker_fd     fd;
-    walker_status file_status;
     walker_uint   file_size;
-    walker_name   path_name;
-    walker_uint   path_length;
     walker_file_function* pFunction;
 };
 
@@ -63,9 +58,6 @@ struct walker_file_function {
     walker_status (*xAppend)    (walker_file*, walker_uint);
     walker_status (*xRead)      (walker_file*, walker_ptr, walker_uint, walker_uint*);
     walker_status (*xWrite)     (walker_file*, walker_ptr, walker_uint, walker_uint*);
-    walker_status (*xLock)      (walker_file*);
-    walker_status (*xUnlock)    (walker_file*);
-    walker_status (*xCheckLock) (walker_file*);
     walker_status (*xSize)      (walker_file*);
 };
 
